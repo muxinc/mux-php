12ca6d2e1b87f04b4ae5cf39e7d1eb8067752c81
Phil Cluff <philcluff@googlemail.com>
2019-05-31 15:41:20 +0100

    Video views, welcome to the partay

---
+++ examples/data/exercise-video-views.php
@@ -0,0 +1,33 @@
+<?php
+
+    assert_options(ASSERT_BAIL, true);
+    require_once 'vendor/autoload.php';
+
+    // Exercises all video vied operations:
+    //   list-video-views
+    //   get-video-view
+
+    // Authentication Setup
+    $config = MuxPhp\Configuration::getDefaultConfiguration()
+        ->setUsername(getenv('MUX_TOKEN_ID'))
+        ->setPassword(getenv('MUX_TOKEN_SECRET'));
+
+    // API Client Initialization
+    $viewsApi = new MuxPhp\Api\VideoViewsApi(
+        new GuzzleHttp\Client(),
+        $config
+    );
+
+    # ========== list-video-views ==========
+    $views = $viewsApi->listVideoViews(["filters" => ['country:GB', 'browser:Chrome'], "timeframe" => ['7:days']]);
+    assert($views->getData() != null);
+    assert(sizeof($views->getData()) > 0);
+    assert($views->getData()[0]->getId() != null);
+    print("list-video-views OK ✅\n");
+
+    # ========== get-video-view ==========
+    $view = $viewsApi->getVideoView($views->getData()[0]->getId());
+    assert($view->getData() != null);
+    assert($view->getData()->getId() != null);
+    print("get-video-view OK ✅\n");
+?>
